e93eea6fed9b70f4b41c3d7cc189890014113528
unknown <Noel Ashford>
2025-02-11 01:45:46 -0500

    Minor Fix #3

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
-  images: {
-    unoptimized: true,
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "s3.ny5.os.tunninet.com",
-        pathname: "/**",
-      },
-    ],
-  },
-  
   experimental: {
-    optimizeCss: false // Prevents Next.js from prefetching Google Fonts (fixes ECONNRESET errors)
+    optimizeCss: false, // Prevents Google Fonts prefetch issue
   },
-  
-  webpack: (config) => config,
 };
 
 export default nextConfig;
